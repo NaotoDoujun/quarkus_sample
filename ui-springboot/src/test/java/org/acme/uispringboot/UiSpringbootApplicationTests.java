582edfae998abfaab8b06d41293310bcf22f4fad
Naoto Doujun <naoto.doujun@gmail.com>
2024-01-16 23:36:35 +0900

    added spring boot graalvm

---
+++ ui-springboot/src/test/java/org/acme/uispringboot/UiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.uispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
