2c17fc814ac9345c9ddac3da2124246f2f4133fb
Naoto Doujun <naoto.doujun@gmail.com>
2024-01-18 17:35:21 +0900

    added js

--- ui-springboot/src/main/java/org/acme/uispringboot/HomeController.java
+++ ui-springboot/src/main/java/org/acme/uispringboot/HomeController.java
@@ -1,12 +1,20 @@
 package org.acme.uispringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private Environment env;
+
     @GetMapping("/home")
-    public String home(){
-        return "home";
+    public String home(Model model){
+        model.addAttribute( "apiBaseUrl", env.getProperty("api.base.url"));
+        return "home.html";
     }
 }
