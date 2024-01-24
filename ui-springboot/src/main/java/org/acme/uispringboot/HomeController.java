85c976da42d7549ce1eb5fbaf7c3ffc7c1eb438b
Naoto Doujun <naoto.doujun@gmail.com>
2024-01-24 21:56:42 +0900

    added Auth0 SAML

--- ui-springboot/src/main/java/org/acme/uispringboot/HomeController.java
+++ ui-springboot/src/main/java/org/acme/uispringboot/HomeController.java
@@ -1,9 +1,11 @@
 package org.acme.uispringboot;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 @Controller
@@ -12,8 +14,17 @@ public class HomeController {
     @Autowired
     private Environment env;
 
-    @GetMapping("/home")
-    public String home(Model model){
+    @RequestMapping("/")
+    public String home(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model){
+        model.addAttribute("name", principal.getName());
+        String email = principal.getFirstAttribute("email");
+        // if email is not preset, try Auth0 attribute name
+        if (email == null) {
+            email = principal.getFirstAttribute("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+        }
+        model.addAttribute("emailAddress", email);
+        model.addAttribute("userAttributes", principal.getAttributes());
+
         model.addAttribute( "apiBaseUrl", env.getProperty("api.base.url"));
         return "home.html";
     }
--- ui-springboot/src/main/java/org/acme/uispringboot/UiSpringbootApplication.java
+++ ui-springboot/src/main/java/org/acme/uispringboot/UiSpringbootApplication.java
@@ -3,17 +3,10 @@ package org.acme.uispringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @SpringBootApplication
 public class UiSpringbootApplication {
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index() {
-		return "redirect:/home";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiSpringbootApplication.class, args);
